49e315975726435274feeb23232baa7043b668ee
Federico <fede.senmartin@gmail.com>
2024-11-15 12:59:21 -0300

    init project commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,10 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import editor from "../../Assets/Projects/ceap.png";
+import chatify from "../../Assets/Projects/bru2.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCode from "../../Assets/Projects/avi.png";
 
 function Projects() {
   return (
@@ -21,40 +21,41 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Smart Lighting Control System"
+              description="Collaborated on a Bluetooth Mesh-based lighting control system, enhancing seamless device communication and scalability. Utilized Android, Kotlin, REST API, Bitrise, and Kotlin Multiplatform technologies."
+              demoLink="https://avi-on.com"
+            />
+          </Col>
+          
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/fedesenmartin/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Digital Bank"
+              description="Contributed to the development and enhancement of a secure and user-friendly digital banking platform. Leveraged technologies such as Android, Kotlin, GraphQL, and Bitrise to deliver a high-performance solution."
+              demoLink="https://play.google.com/store/apps/details?id=com.brubank&hl=en_EN"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/fedesenmartin/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+   
 
-          <Col md={4} className="project-card">
+         <Col md={4} className="project-card">
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/fedesenmartin/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Aviation trainig"
+              description="Developed landing page and Improved software tools for a flight school, optimizing workflows for scheduling and student management. Worked with PHP and AWS to ensure scalability and reliability."
+              // ghLink="https://github.com/fedesenmartin/Editor.io"
+              demoLink="https://ceap.com.ar"              
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
@@ -63,9 +64,9 @@ function Projects() {
               ghLink="https://github.com/fedesenmartin/Plant_AI"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
-          </Col>
+          </Col> */}
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
@@ -74,9 +75,9 @@ function Projects() {
               ghLink="https://github.com/fedesenmartin/AI_For_Social_Good"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
-          </Col>
+          </Col> */}
 
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
@@ -86,7 +87,7 @@ function Projects() {
               ghLink="https://github.com/fedesenmartin/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col>
+          </Col> */}
         </Row>
       </Container>
     </Container>
