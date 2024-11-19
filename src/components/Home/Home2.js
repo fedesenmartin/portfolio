60642ae6ca6626dc6e7534e7dff2d4f39940ddb0
Federico <fede.senmartin@gmail.com>
2024-11-19 15:03:00 -0300

    rounded image

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/icon.png";
+import myImg from "../../Assets/icon2.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
